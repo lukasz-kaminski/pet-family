09cb713d65886dc63c117420ef0c86faa308b210
Łukasz Kamiński <lkamins7@gmail.com>
2021-09-02 21:05:30 +0200

    Initial commit

---
+++ src/main/java/cx/kaminski/petfamily/PetFamilyApplication.java
@@ -0,0 +1,13 @@
+package cx.kaminski.petfamily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetFamilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetFamilyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cx/kaminski/petfamily/PetFamilyApplicationTests.java
@@ -0,0 +1,13 @@
+package cx.kaminski.petfamily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetFamilyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
